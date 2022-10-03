37e87c2eca739bf8738b32d70f5fa2c74eaa79ff
Vadym Chutur <falloutgreck@gmail.com>
2022-10-03 15:04:18 +0300

    update 08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,19 @@
 // Add imports above this line
+import galleryCArdTpl from '../templates/01-gallery.hbs';
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const imagTumbInGalery = document.querySelector('.gallery');
+
+imagTumbInGalery.innerHTML = createGalleryItemsMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryCArdTpl(galleryItems);
+}
